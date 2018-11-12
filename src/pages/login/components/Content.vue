ae377521e3f98c2909d7a92e23d3970d0155b6a7
Derek <463645977@qq.com>
2018-11-12 11:57:57 +0800

    登陆界面添加了验证码

--- src/pages/login/components/Content.vue
+++ src/pages/login/components/Content.vue
@@ -2,8 +2,8 @@
   <div class="content">
     <div class="content__user">
       <p class="content__user_title">共享插座后台管控系统</p>
-      <span v-if="userStatus === 'invalid'" class="content__user_errorTips">用户名或密码错误</span>
       <div class="content__user_form">
+        <span v-show="userStatus === 'invalid'" class="content__user_form_errorTips">用户名或密码错误</span>
         <el-input
         placeholder="请输入用户名"
         v-model="username"
@@ -15,10 +15,20 @@
         type="password"
         clearable class="content__user_form__password">
         </el-input>
-        <el-button plain @click="getUserLogin">登陆</el-button>
+        <div class="content__usere_form__ccode">
+          <el-input
+          placeholder="验证码"
+          v-model="vcode"
+          clearable class="content__user_form__ccode_ipt">
+          </el-input>
+          <span class="content__user_form__ccode_text" @click="generatedCode">
+          {{this.ccode}}
+          </span>
+        </div>
+        <el-button class="content__user_form__btn-login" plain @click="checkCode">登陆</el-button>
         <div class="content__user_form__ext">
           <el-button type="text" class="content__user_form__ext__register" @click="userRegister">注册</el-button>
-          <span class="content__user_form__ext__forgetPassword" @click="forgetPassword">忘记密码</span>
+          <el-button type="text" class="content__user_form__ext__forgetPassword" @click="forgetPassword">忘记密码</el-button>
         </div>
       </div>
     </div>
@@ -28,6 +38,7 @@
 <script>
 import {mapGetters} from 'vuex'
 import Steps from './Steps'
+// import { getUserLogin } from '../../../api/getUserLogin';
 export default {
   name: 'Content',
   components:{
@@ -36,7 +47,9 @@ export default {
   data() {
     return{
       username:'',
-      password:''
+      password:'',
+      ccode:'',
+      vcode:''
     }
   },
   computed:{
@@ -44,8 +57,34 @@ export default {
       userStatus:'getUserStatus'
     })
   },
-  
+  mounted(){
+    this.generatedCode();
+  },
   methods:{
+    generatedCode () {
+      const random = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+      let code = ''
+      for (let i = 0; i < 4; i++) {
+        let index = Math.floor(Math.random() * 36)
+        code += random[index]
+      }
+      this.ccode = code
+    },
+    // 判断验证码是否输入准确
+    checkCode () {
+      let vcode = this.vcode
+      vcode = vcode.toUpperCase()
+      let ccode = this.ccode
+      ccode = ccode.toUpperCase()
+      if (vcode !== ccode) {
+        this.$message.error('Please enter the correct verification code!')
+        // this.$set(this.loginInfo, 'vcode', '')
+        // this.$set(this.loginInfo, 'password', '')
+      } else {
+        this.getUserLogin();
+      }
+    },
     userRegister(){
         this.$alert(<steps></steps>, '用户注册', {
           showConfirmButton:false,
@@ -81,21 +120,37 @@ export default {
       width 30%
       height 60%
       border .2rem solid #ededed
-      &_errorTips
-        padding-bottom .2rem
-        color red
       &_title
         font-size .5rem
-        margin-bottom .5rem
+        margin-bottom .7rem
       &_form
         width 50%
         display flex
         flex-direction column
         align-items center
+        &_errorTips
+          padding-bottom .1rem
+          color red
         &__username
           padding-bottom .1rem
         &__password
           padding-bottom .1rem
+        &__ccode
+          flex-direction row
+          &_ipt
+            width 50%
+            padding-bottom .1rem
+          &_text
+            display inline-block
+            width 45%
+            height .4rem
+            margin-left .1rem
+            padding-top .1rem
+            text-align center
+            background-color #ededed
+        &__btn-login
+          width 100%
+          background-color #ededed
         &__ext
           width 100%
           font-size .05rem
