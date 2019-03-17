73b0147fa15ae2bd21fc702da1347495f7d29647
Derek <463645977@qq.com>
2019-03-17 17:38:30 +0800

    修改了 vue.config

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // open: true,
     // https: false,
     // hotOnly: false,
-    port: 8081,
+    port: 8080,
     disableHostCheck: true, // 处理 host 不识别问题
     // proxy: 'http://localhost:8080'
     proxy: 'http://47.112.195.182'
