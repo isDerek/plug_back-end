468c1a8173576be1d95c9ac730d2b2eb187e4ec4
Derek <463645977@qq.com>
2019-03-19 22:19:35 +0800

    修改了 config 端口为 8080 ,只影响本地调试

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,6 @@ module.exports = {
     port: 8080,
     disableHostCheck: true, // 处理 host 不识别问题
     // proxy: 'http://localhost:8080'
-    proxy: 'http://47.112.195.182'
+    proxy: 'http://47.112.195.182:8080'
   }
 }
